aa3af737fbfb468808ff16f624e11532a5233acf
Cheick Keita <kcheick@gmail.com>
2021-01-25 10:57:33 -0800

    introducing bugs - stack-buffer-overflow - bad-free

--- examples/simple-libfuzzer/simple.c
+++ examples/simple-libfuzzer/simple.c
@@ -28,6 +28,16 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t len) {
         int* p = &cnt - 32; for (int i = 0; i < 32; i++) { *(p + i) = 0; }
         break;
       }
+      case '2': {
+        // stack-buffer-overflow
+        int* p = &cnt + 32; for (int i = 0; i < 32; i++) { *(p - i) = 0; }
+        break;
+      }
+      case '3': {
+        // bad-free
+        int *p = &cnt; free(p);
+        break;
+      }
     }
   }
 
