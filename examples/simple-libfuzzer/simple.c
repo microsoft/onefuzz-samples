f68ba97e72f51ae42a546422f61c4c399ff512ab
Cheick Keita <kcheick@gmail.com>
2021-01-25 10:56:44 -0800

    introducing bugs: - segv - stack-buffer-underflow

--- examples/simple-libfuzzer/simple.c
+++ examples/simple-libfuzzer/simple.c
@@ -12,5 +12,24 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t len) {
     return 1;
   }
 
+  if (data[0] == 'x') { cnt++; }
+  if (data[1] == 'y') { cnt++; }
+  if (data[2] == 'z') { cnt++; }
+
+  if (cnt >= 3) {
+    switch (data[3]) {
+      case '0': {
+        // segv
+        int *p = NULL; *p = 123;
+        break;
+      }
+      case '1': {
+        // stack-buffer-underflow
+        int* p = &cnt - 32; for (int i = 0; i < 32; i++) { *(p + i) = 0; }
+        break;
+      }
+    }
+  }
+
   return 0;
 }
